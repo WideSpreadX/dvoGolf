621db207b7e6d651ae96a9938fb19260a28fcf1e
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2020-08-24 14:21:45 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const Handlebars = require('handlebars')
+const exphbs = require("express-handlebars");
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+/* app.use(express.urlencoded({extended: true})); */
+app.use(express.json());
+app.use(express.static('public'));
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/golf', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Landing Page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Register Page
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// Logout Page
+app.post('/logout', (req, res) => {
+    res.render('/');
+});
+
+// Food & Drink Menu Page
+app.get('/food-drink', (req, res) => {
+    res.render('food-drink');
+});
+
+// Home Page
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+// Scorecard Page
+app.get('/scorecard', (req, res) => {
+    res.render('scorecard');
+});
+
+// Help Page
+app.get('/help', (req, res) => {
+    res.render('help');
+});
+
+// Start Server
+app.listen(PORT, function() {
+    console.log(`App running on port ${PORT}`);
+})
